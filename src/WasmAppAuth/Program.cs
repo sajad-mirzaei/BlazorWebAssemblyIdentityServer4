961007e85ef9445597a19adb1dee0d1b2ce2d33b
Sajad <mirzaei.sajad@hotmail.com>
2022-03-08 13:02:43 +0330

    Upload project files

---
+++ src/WasmAppAuth/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WasmAppAuth
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient{ BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                // Configure your authentication provider options here.
+                // For more information, see https://aka.ms/blazor-standalone-auth
+                options.ProviderOptions.Authority = "https://localhost:5001";
+                options.ProviderOptions.ClientId = "wasmappauth-client";
+                options.ProviderOptions.ResponseType = "code";
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
